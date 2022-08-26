fca6109668618cd13e601097dcd7fbf33e62a513
nmatzke <matzke@berkeley.edu>
2022-08-26 16:18:53 +1200

    Update run_inf_v01.jl

--- ex/siminf1/ss8_simulation_001/run_inf_v01.jl
+++ ex/siminf1/ss8_simulation_001/run_inf_v01.jl
@@ -154,11 +154,17 @@ Es_interpolator(1.0)
 
 
 #######################################################
-# Optimize the DEC model (fixed birthRate and deathRate)
+#######################################################
+#######################################################
+# Optimize the DEC model (fixed birthRate and deathRate=0.0)
+#######################################################
+#######################################################
 #######################################################
 
 pars = bmo.est[bmo.type .== "free"]
 parnames = bmo.rownames[bmo.type .== "free"]
+lower = bmo.min[bmo.type .== "free"]
+upper = bmo.max[bmo.type .== "free"]
 func = x -> func_to_optimize(x, parnames, inputs, p_Ds_v5; returnval="bgb_lnL", printlevel=1)
 pars = [0.9, 0.9]
 func(pars)
@@ -185,4 +191,184 @@ opt.upper_bounds
 opt.ftol_abs = 0.00001 # tolerance on log-likelihood
 (optf,optx,ret) = NLopt.optimize!(opt, pars)
 #######################################################
+
+
+
+
+
+
+# Get the inputs & "res" results object:
+pars = optx
+inputs.bmo.est[inputs.bmo.type .== "free"] .= pars
+bmo_updater_v1!(inputs.bmo)
+p_Ds_v5_updater_v1!(p_Ds_v5, inputs);
+
+# Solve the Es
+print("\nSolving the Es once, for the whole tree timespan...")
+prob_Es_v5 = DifferentialEquations.ODEProblem(parameterized_ClaSSE_Es_v7_simd_sums, p_Ds_v5.uE, Es_tspan, p_Ds_v5);
+# This solution is an interpolator
+sol_Es_v5 = solve(prob_Es_v5, solver_options.solver, save_everystep=solver_options.save_everystep, abstol=solver_options.abstol, reltol=solver_options.reltol);
+Es_interpolator = sol_Es_v5;
+prtCp(p_Ds_v5)
+p_Ds_v7 = (n=p_Ds_v5.n, params=p_Ds_v5.params, p_indices=p_Ds_v5.p_indices, p_TFs=p_Ds_v5.p_TFs, uE=p_Ds_v5.uE, sol_Es_v5=sol_Es_v5);
+prtCp(p_Ds_v5)
+
+(total_calctime_in_sec, iteration_number, Julia_sum_lq, rootstates_lnL, Julia_total_lnLs1, bgb_lnL) = iterative_downpass_nonparallel_ClaSSE_v7!(inputs.res; trdf=trdf, p_Ds_v7=p_Ds_v7, solver_options=inputs.solver_options, max_iterations=10^5, return_lnLs=true)
+
+# The approximate ancestral state probabilities are in the "res" results object here:
+# (I say "approximate" because the normalized downpass state likelihoods are not quite
+#  identical with Felsenstein's ancestral state probabilities, which require an uppass as
+#  well as a downpass; but they are typically the dominant influence.)
+#
+Rnames(res)
+
+# E.g. root state probabilities:
+round.(res.normlikes_at_each_nodeIndex_branchTop[tr.root], digits=3)
+# The rows are the nodes as listed in the tree table at prt(tr)
+# The columns are the 8 ranges: null, A, B, C, AB, AC, BC, ABC
+
+
+
+
+
+
+
+
+
+#######################################################
+#######################################################
+#######################################################
+# ML inference on DEC + birth-death
+#######################################################
+#######################################################
+#######################################################
+bmo = construct_BioGeoBEARS_model_object()
+bmo.type[bmo.rownames .== "j"] .= "fixed"
+bmo.type[bmo.rownames .== "birthRate"] .= "free"
+bmo.type[bmo.rownames .== "deathRate"] .= "free"
+bmo.est[bmo.rownames .== "birthRate"] .= 0.3288164
+bmo.est[bmo.rownames .== "deathRate"] .= 0.1
+bmo.est[bmo.rownames .== "d"] .= 0.03505038
+bmo.est[bmo.rownames .== "e"] .= 0.02832370
+bmo.est[bmo.rownames .== "a"] .= 0.0
+bmo.est[bmo.rownames .== "j"] .= 0.0
+bmo.est[:] = bmo_updater_v1(bmo) # works
+
+
+inputs = PhyBEARS.ModelLikes.setup_DEC_SSE2(numareas, tr, geog_df; root_age_mult=1.5, max_range_size=NaN, include_null_range=true, bmo=bmo);
+(setup, res, trdf, bmo, solver_options, p_Es_v5, Es_tspan) = inputs;
+
+prtCp(p_Es_v5)
+
+# Problem: this simulation allowed null-range speciation (null->null, null or 000->000,000)
+# Solution for now: Add the null -> null, null cladogenesis event to the p_Es_v5
+birthRate = bmo.est[bmo.rownames .== "birthRate"]
+add_111_to_Carray!(p_Es_v5, birthRate)
+
+prtCp(p_Es_v5) # now 1->1,1 is an allowed cladogenesis event
+
+
+inputs.bmo.type[bmo.rownames .== "j"] .= "fixed"
+parnames = bmo.rownames[bmo.type .== "free"]
+lower = bmo.min[bmo.type .== "free"]
+upper = bmo.max[bmo.type .== "free"]
+
+pars = bmo.est[bmo.type .== "free"]
+bmo_updater_v1!(inputs.bmo) # works
+inputs.bmo
+
+prtCp(p_Es_v5)
+p_Ds_v5_updater_v1!(p_Es_v5, inputs);  # WORKS 2022-03-10
+prtCp(p_Es_v5)
+
+# Solve the Es
+print("\nSolving the Es once, for the whole tree timespan...")
+prob_Es_v5 = DifferentialEquations.ODEProblem(PhyBEARS.SSEs.parameterized_ClaSSE_Es_v7_simd_sums, p_Es_v5.uE, Es_tspan, p_Es_v5);
+# This solution is an interpolator
+sol_Es_v5 = solve(prob_Es_v5, solver_options.solver, save_everystep=solver_options.save_everystep, abstol=solver_options.abstol, reltol=solver_options.reltol);
+Es_interpolator = sol_Es_v5;
+p_Ds_v5 = (n=p_Es_v5.n, params=p_Es_v5.params, p_indices=p_Es_v5.p_indices, p_TFs=p_Es_v5.p_TFs, uE=p_Es_v5.uE, terms=p_Es_v5.terms, sol_Es_v5=sol_Es_v5);
+
+# Check the interpolator
+p_Ds_v5.sol_Es_v5(1.0)
+Es_interpolator(1.0)
+
+# Calculate the Ds & total lnL via downpass
+(total_calctime_in_sec, iteration_number, Julia_sum_lq, rootstates_lnL, Julia_total_lnLs1, bgb_lnL) = PhyBEARS.TreePass.iterative_downpass_nonparallel_ClaSSE_v7!(res; trdf=trdf, p_Ds_v7=p_Ds_v5, solver_options=inputs.solver_options, max_iterations=10^5, return_lnLs=true)
+
+pars = bmo.est[bmo.type .== "free"]
+#func = x -> func_to_optimize(x, parnames, inputs, p_Ds_v5; returnval="bgb_lnL")
+func = x -> func_to_optimize(x, parnames, inputs, p_Ds_v5; returnval="lnL")
+
+pars = [0.9, 0.9, 0.3, 0.2]
+func(pars)
+function func2(pars, dummy_gradient!)
+	return func(pars)
+end # END function func2(pars, dummy_gradient!)
+
+
+#######################################################
+# Best optimizer so far - 2022-03-15
+#######################################################
+using NLopt
+func(pars)
+opt = NLopt.Opt(:LN_BOBYQA, length(pars))
+ndims(opt)
+opt.algorithm
+algorithm_name(opt::Opt)
+opt.min_objective = func2
+opt.lower_bounds = lower::Union{AbstractVector,Real}
+opt.upper_bounds = upper::Union{AbstractVector,Real}
+opt.lower_bounds
+opt.upper_bounds
+opt.ftol_abs = 0.00001 # tolerance on log-likelihood
+(optf,optx,ret) = NLopt.optimize!(opt, pars)
+#######################################################
+
+
+
+
+
+# Get the inputs & res:
+pars = optx
+inputs.bmo.est[inputs.bmo.type .== "free"] .= pars
+inputs_updater_v1!(inputs)
+p_Ds_v5_updater_v1!(p_Ds_v5, inputs);
+
+printlevel=1
+returnval="bgb_lnL"
+func(pars);
+
+# Solve the Es
+print("\nSolving the Es once, for the whole tree timespan...")
+prob_Es_v5 = DifferentialEquations.ODEProblem(parameterized_ClaSSE_Es_v7_simd_sums, p_Ds_v5.uE, Es_tspan, p_Ds_v5);
+# This solution is an interpolator
+sol_Es_v5 = solve(prob_Es_v5, solver_options.solver, save_everystep=true, abstol=solver_options.abstol, reltol=solver_options.reltol);
+Es_interpolator = sol_Es_v5;
+prtCp(p_Ds_v5)
+p_Ds_v7 = (n=p_Ds_v5.n, params=p_Ds_v5.params, p_indices=p_Ds_v5.p_indices, p_TFs=p_Ds_v5.p_TFs, uE=p_Ds_v5.uE, sol_Es_v5=sol_Es_v5);
+prtCp(p_Ds_v5)
+
+
+(total_calctime_in_sec, iteration_number, Julia_sum_lq, rootstates_lnL, Julia_total_lnLs1, bgb_lnL) = iterative_downpass_nonparallel_ClaSSE_v7!(inputs.res; trdf=trdf, p_Ds_v7=p_Ds_v7, solver_options=inputs.solver_options, max_iterations=10^5, return_lnLs=true)
+
+# The approximate ancestral state probabilities are in the "res" results object here:
+# (I say "approximate" because the normalized downpass state likelihoods are not quite
+#  identical with Felsenstein's ancestral state probabilities, which require an uppass as
+#  well as a downpass; but they are typically the dominant influence.)
+#
+Rnames(res)
+
+# E.g. root state probabilities:
+round.(res.normlikes_at_each_nodeIndex_branchTop[tr.root], digits=3)
+# The rows are the nodes as listed in the tree table at prt(tr)
+# The columns are the 8 ranges: null, A, B, C, AB, AC, BC, ABC
+
+
+
+
+
+
+
+
 
