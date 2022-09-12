f3ddb17e887c25922bf7189e0439bfef34951f28
nmatzke <matzke@berkeley.edu>
2022-09-12 16:35:21 +1200

    Update TimeDep.jl

--- src/TimeDep.jl
+++ src/TimeDep.jl
@@ -163,7 +163,7 @@ function update_Qij_e_vals!(p)
 	#Rnames(p.p_indices)
 	# PRE-ALLOCATE THIS FOR SPEED
 	#e_rows = (1:length(p.p_indices.Qarray_event_types))[p.p_indices.Qarray_event_types .== "e"]
-	@inbounds @simd for i in 1:length(p.setup.e_rows)
+	@inbounds @simd for i in 1:length(p.p_indices.e_rows)
 		#starting_statenum = p.p_indices.Qarray_ivals[p.setup.e_rows[i]]
 		#ending_statenum = p.p_indices.Qarray_jvals[p.setup.e_rows[i]]
 		#area_lost = symdiff(p.setup.states_list[starting_statenum], p.setup.states_list[ending_statenum])
@@ -174,8 +174,8 @@ function update_Qij_e_vals!(p)
 		
 		# actual rate of e = base_rate_of_e * area_of_area_lost ^ u
 		#p.params.Qij_vals_t[p.setup.e_rows[i]] = p.params.Qij_vals[p.setup.e_rows[i]] * p.setup.elist_t[area_lost][]
-		p.params.Qij_vals[p.setup.e_rows[i]] = p.setup.elist_t[p.setup.losses[p.setup.e_rows[i][1]] ][1]
-		p.params.Qij_vals_t[p.setup.e_rows[i]] = p.setup.elist_t[p.setup.losses[p.setup.e_rows[i][1]] ][1]
+		p.params.Qij_vals[p.p_indices.e_rows[i]] = p.setup.elist_t[p.p_indices.losses[p.p_indices.e_rows[i][1]] ][1]
+		p.params.Qij_vals_t[p.p_indices.e_rows[i]] = p.setup.elist_t[p.p_indices.losses[p.p_indices.e_rows[i][1]] ][1]
 	end
 	
 	
