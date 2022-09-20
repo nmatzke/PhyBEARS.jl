7935c3ddfb1af94684a8c2a145665f14bc7db3df
nmatzke <matzke@berkeley.edu>
2022-09-20 18:53:43 +1200

    p.setup.u_mu_row -> p.setup.bmo_rows.u_e

--- src/SSEs.jl
+++ src/SSEs.jl
@@ -1041,7 +1041,7 @@ parameterized_ClaSSE_Es_v10_simd_sums = (du,u,p,t) -> begin
   # Populate changing mus with time
   @inbounds @simd for i in 1:n
   	# total_area = get_area_of_range(tval, state_as_areas_list, area_of_areas_interpolator)
-  	mu_t[i] = mu[i] * get_area_of_range(t, p.states_as_areas_lists[i], p.setup.area_of_areas)^p.bmo.est[p.setup.u_mu_row]
+  	mu_t[i] = mu[i] * get_area_of_range(t, p.states_as_areas_lists[i], p.setup.area_of_areas)^p.bmo.est[p.setup.bmo_rows.u_e]
   end
   # Correct "Inf" max_extinction_rates
   mu_t[mu_t .> max_extinction_rate] .= max_extinction_rate
@@ -1086,7 +1086,7 @@ parameterized_ClaSSE_Ds_v10_simd_sums = (du,u,p,t) -> begin
   # Populate changing mus with time
   @inbounds @simd for i in 1:n
   	# total_area = get_area_of_range(tval, state_as_areas_list, area_of_areas_interpolator)
-  	mu_t[i] = mu[i] * get_area_of_range(t, p.states_as_areas_lists[i], p.setup.area_of_areas)^p.bmo.est[p.setup.u_mu_row]
+  	mu_t[i] = mu[i] * get_area_of_range(t, p.states_as_areas_lists[i], p.setup.area_of_areas)^p.bmo.est[p.setup.bmo_rows.u_e]
   end
   # Correct "Inf" max_extinction_rates
   mu_t[mu_t .> max_extinction_rate] .= max_extinction_rate
