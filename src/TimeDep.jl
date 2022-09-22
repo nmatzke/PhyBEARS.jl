40d3f0f4bb724c8baf3a96ae3b81a162fc2ad5c2
Nicholas J. Matzke <nickmatzke.ncse@gmail.com>
2022-09-22 16:38:39 +1200

    Update TimeDep.jl

--- src/TimeDep.jl
+++ src/TimeDep.jl
@@ -162,7 +162,7 @@ function update_Qij_e_vals!(p)
 	#end
 	
 	#p.setup.elist_t .= p.setup.elist_base .* p.setup.area_of_areas.^p.bmo.est[p.setup.bmo_rows.u_e]
-	@inbounds @simd for i in 1:length(p.setup.elist_t[i])
+	@inbounds @simd for i in 1:length(p.setup.elist_t)
 		p.setup.elist_t[i] = p.setup.elist_base * p.setup.area_of_areas^p.bmo.est[p.setup.bmo_rows.u_e]
 	end
 	
