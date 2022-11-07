c67d55b399474aa1ee36e0fd7fb2cf3e894bad2d
nmatzke <matzke@berkeley.edu>
2022-11-07 16:33:12 +1300

    Update example_readfiles_v1.jl

--- files/example_readfiles_v1.jl
+++ files/example_readfiles_v1.jl
@@ -4,23 +4,23 @@ distmats = parse_distances_fn(fn)
 fn = "/GitHub/PhyBEARS.jl/files/distances_changing_v1.txt";
 distmats = parse_distances_fn(fn)
 
-fn = "/Users/nickm/GitHub/PhyBEARS.jl/files/area_of_areas_changing_v1_wEND.txt";
+fn = "/GitHub/PhyBEARS.jl/files/area_of_areas_changing_v1_wEND.txt";
 area_vectors = parse_areas_fn(fn)
 
-fn = "/Users/nickm/GitHub/PhyBEARS.jl/files/area_of_areas_changing_v1.txt";
+fn = "/GitHub/PhyBEARS.jl/files/area_of_areas_changing_v1.txt";
 area_vectors = parse_areas_fn(fn)
 
-fn = "/Users/nickm/GitHub/PhyBEARS.jl/files/times_v1.txt";
+fn = "/GitHub/PhyBEARS.jl/files/times_v1.txt";
 times = parse_times_fn(fn)
 
-fn = "/Users/nickm/GitHub/PhyBEARS.jl/files/times_v1.txt";
+fn = "/GitHub/PhyBEARS.jl/files/times_v1.txt";
 times = parse_numbers_list_fn(fn)
 
-fn = "/Users/nickm/GitHub/PhyBEARS.jl/files/times_v1_no0time.txt";
+fn = "/GitHub/PhyBEARS.jl/files/times_v1_no0time.txt";
 times = parse_times_fn(fn)
 
-fn = "/Users/nickm/GitHub/PhyBEARS.jl/files/times_v1_no0time.txt";
+fn = "/GitHub/PhyBEARS.jl/files/times_v1_no0time.txt";
 times = parse_numbers_list_fn(fn)
 
-fn = "/Users/nickm/GitHub/PhyBEARS.jl/files/times_wEND_v1.txt";
+fn = "/GitHub/PhyBEARS.jl/files/times_wEND_v1.txt";
 times = parse_times_fn(fn)
