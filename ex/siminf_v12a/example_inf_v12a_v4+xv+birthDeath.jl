13832a7e906a28d06e4698722212dda9cd63a94b
nmatzke <matzke@berkeley.edu>
2022-11-11 16:14:42 +1300

    working

--- ex/siminf_v12a/example_inf_v12a_v4+xv+birthDeath.jl
+++ ex/siminf_v12a/example_inf_v12a_v4+xv+birthDeath.jl
@@ -21,16 +21,16 @@ using DataFrames
 using CSV
 
 # Change the working directory as needed
-wd = "/GitHub/PhyBEARS.jl/data/"
+wd = "/GitHub/PhyBEARS.jl/ex/siminf_v12a/"
 cd(wd)
 
 # This simulation has 50 living species
-trfn = "Psychotria_tree.newick"
+trfn = "tree.newick"
 tr = readTopology(trfn)
 trdf = prt(tr)
 oldest_possible_age = 100.0
 
-lgdata_fn = "Psychotria_geog.data"
+lgdata_fn = "rangedata.data"
 geog_df = Parsers.getranges_from_LagrangePHYLIP(lgdata_fn);
 include_null_range = true
 numareas = Rncol(geog_df)-1
@@ -61,13 +61,9 @@ inputs = PhyBEARS.ModelLikes.setup_DEC_SSE2(numareas, tr, geog_df; root_age_mult
 #######################################################
 # Read in and parse distances and area-of-areas
 #######################################################
-files.times_fn = "/Users/nickm/GitHub/PhyBEARS.jl/files/v12a_times.txt"
-files.distances_fn = "/Users/nickm/GitHub/PhyBEARS.jl/files/v12a_distances.txt"
-files.area_of_areas_fn = "/Users/nickm/GitHub/PhyBEARS.jl/files/v12a_area_of_areas.txt"
-
-files.times_fn = "Hawaii_KOMH_times.txt"
-files.distances_fn = "Hawaii_KOMH_distances_changing_fractions_PhyBEARS.txt"
-files.area_of_areas_fn = ""
+files.times_fn = "/GitHub/PhyBEARS.jl/files/v12a_times.txt"
+files.distances_fn = "/GitHub/PhyBEARS.jl/files/v12a_distances.txt"
+files.area_of_areas_fn = "/GitHub/PhyBEARS.jl/files/v12a_area_of_areas.txt"
 
 # Construct interpolators, times-at-which-to-interpolate QC
 p = p_Ds_v5
@@ -91,11 +87,13 @@ p = p_Ds_v12 = (n=p_Es_v12.n, params=p_Es_v12.params, p_indices=p_Es_v12.p_indic
 #######################################################
 # Maximum likelihood inference
 #######################################################
+bmo.type[bmo.rownames .== "birthRate"] .= "free"
+bmo.type[bmo.rownames .== "deathRate"] .= "free"
 bmo.type[bmo.rownames .== "xv"] .= "free"
 pars = bmo.est[bmo.type .== "free"]
 parnames = bmo.rownames[bmo.type .== "free"]
 func = x -> func_to_optimize_v12(x, parnames, inputs, p_Ds_v12; returnval="bgb_lnL", printlevel=1)
-pars = [0.9, 0.9, 0.1]
+pars = [0.4, 0.001, bmo.est[bmo.rownames .== "birthRate"][1], bmo.est[bmo.rownames .== "deathRate"][1], 0.1]
 func(pars)
 function func2(pars, dummy_gradient!)
 	return func(pars)
@@ -112,9 +110,9 @@ lower = bmo.min[bmo.type .== "free"]
 upper = bmo.max[bmo.type .== "free"]
 opt.lower_bounds = lower::Union{AbstractVector,Real}
 opt.upper_bounds = upper::Union{AbstractVector,Real}
-opt.ftol_abs = 0.01 # tolerance on log-likelihood
+#opt.ftol_abs = 0.01 # tolerance on log-likelihood
 #opt.ftol_rel = 0.01 # tolerance on log-likelihood
-opt.xtol_abs = 0.0001 # tolerance on parameters
+#opt.xtol_abs = 0.0001 # tolerance on parameters
 #opt.xtol_rel = 0.001 # tolerance on parameters
 (optf,optx,ret) = NLopt.optimize!(opt, pars)
 #######################################################
@@ -124,11 +122,20 @@ opt.xtol_abs = 0.0001 # tolerance on parameters
 
 # Get the inputs & res:
 pars = optx
-#pars = [0.9747407112459348, 0.8, 0.11]
-#pars = [100.0, 1.8, 0.11]
 inputs.bmo.est[inputs.bmo.type .== "free"] .= pars
 bmo_updater_v1!(inputs.bmo)
-p_Ds_v5_updater_v1!(p_Ds_v12, inputs);
 res = inputs.res
 
+# Solution, under best ML parameters
+p_Ds_v5_updater_v1!(p_Ds_v12, inputs);
+p_Es_v12 = TimeDep.construct_QC_interpolators(p_Ds_v12, p_Ds_v12.interpolators.times_for_SSE_interpolators);
+
+# Solve the Es
+prob_Es_v12 = DifferentialEquations.ODEProblem(parameterized_ClaSSE_Es_v12_simd_sums, p_Es_v12.uE, inputs.Es_tspan, p_Es_v12)
+# This solution is an interpolator
+sol_Es_v12 = solve(prob_Es_v12, inputs.solver_options.solver, save_everystep=inputs.solver_options.save_everystep, abstol=inputs.solver_options.abstol, reltol=inputs.solver_options.reltol);
+p_Ds_v12 = (n=p_Es_v12.n, params=p_Es_v12.params, p_indices=p_Es_v12.p_indices, p_TFs=p_Es_v12.p_TFs, uE=p_Es_v12.uE, terms=p_Es_v12.terms, setup=p_Es_v12.setup, states_as_areas_lists=p_Es_v12.states_as_areas_lists, use_distances=p_Es_v12.use_distances, bmo=p_Es_v12.bmo, interpolators=p_Es_v12.interpolators, sol_Es_v12=sol_Es_v12);
+
+# Calculate the Ds
+(total_calctime_in_sec, iteration_number, Julia_sum_lq, rootstates_lnL, Julia_total_lnLs1, bgb_lnL) = iterative_downpass_nonparallel_ClaSSE_v12!(res; trdf=trdf, p_Ds_v12=p_Ds_v12, solver_options=inputs.solver_options, max_iterations=10^6, return_lnLs=true)
 
