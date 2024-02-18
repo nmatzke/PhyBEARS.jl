5534b81089e0bffe090ecc5ec0a7da65ca6e206b
nmatzke <matzke@berkeley.edu>
2024-02-19 12:06:48 +1300

    slight update startup.jl

---
+++ setup/_install_from_scratch_v1.jl
@@ -0,0 +1,56 @@
+
+Reinstall from scratch
+
+import Pkg
+using Pkg
+
+Pkg.add("Compat")
+Pkg.add("Preferences")
+
+
+Pkg.add("UUIDs")
+Pkg.add("CSV")
+Pkg.add("FilePathsBase")
+Pkg.add("SciMLBase")
+Pkg.add("DataFrames")
+Pkg.add("")
+Pkg.add("")
+Pkg.add("")
+Pkg.add("")
+Pkg.add("")
+Pkg.add("")
+Pkg.add("")
+Pkg.add("BenchmarkTools")
+
+
+using Compat
+using Preferences
+using UUIDs
+
+using CSV
+using FilePathsBase
+
+using SciMLBase
+
+using 
+using 
+using 
+using 
+using 
+using 
+using 
+using 
+using 
+using 
+using 
+using 
+using 
+using 
+using 
+using 
+using 
+using 
+using 
+using 
+using 
+using 
