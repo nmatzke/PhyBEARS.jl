07a5c15d99a14c8f333015cf8fb78930ec9b77a6
Nicholas J. Matzke <nickmatzke.ncse@gmail.com>
2024-10-18 15:48:27 +1300

    trying Rdata

---
+++ ex/cicadidae2/phybears_M0_mr3/lnLs_tuple.R
@@ -0,0 +1,8 @@
+input_tuple = list()
+input_tuple$total_calctime_in_sec = 4.745
+input_tuple$iteration_number = 16
+input_tuple$Julia_sum_lq = -993.4815226741605
+input_tuple$rootstates_lnL = -9.074690043301379
+input_tuple$Julia_total_lnLs1 = -1002.5562127174619
+input_tuple$bgb_lnL = -310.9375694890624
+input_tuple$total_loglikelihood = -310.9375694890624
